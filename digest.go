0b0daa96168fba0367e8e1bb1ad3b6e179215c6c
Hank Donnay <hdonnay@redhat.com>
2020-01-28 13:48:47 -0500

    claircore: switch fundamental types to Digest

--- digest.go
+++ digest.go
@@ -2,19 +2,41 @@ package claircore
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"crypto/sha512"
 	"database/sql/driver"
 	"encoding/hex"
 	"fmt"
+	"hash"
 )
 
+// Digest is a type representing the hash of some data.
+//
+// It's used throughout claircore packages as an attempt to remain independent
+// of a specific hashing algorithm.
 type Digest struct {
 	algo     string
 	checksum []byte
 }
 
+// Checksum returns the checksum byte slice.
 func (d Digest) Checksum() []byte { return d.checksum }
 
+// Algorithm returns a string representation of the algorithm used for this
+// digest.
 func (d Digest) Algorithm() string { return d.algo }
+
+// Hash returns an instance of the hashing algorithm used for this Digest.
+func (d Digest) Hash() hash.Hash {
+	switch d.algo {
+	case "sha256":
+		return sha256.New()
+	case "sha512":
+		return sha512.New()
+	default:
+		panic("Hash() called on an invalid Digest")
+	}
+}
 
 func (d Digest) String() string {
 	b, _ := d.MarshalText()
@@ -36,14 +58,51 @@ func (d Digest) MarshalText() ([]byte, error) {
 func (d *Digest) UnmarshalText(t []byte) error {
 	i := bytes.IndexByte(t, ':')
 	if i == -1 {
-		return fmt.Errorf("invalid digest format")
+		return &DigestError{msg: "invalid digest format"}
 	}
 	d.algo = string(t[:i])
 	t = t[i+1:]
-	d.checksum = make([]byte, hex.DecodedLen(len(t)))
-	if _, err := hex.Decode(d.checksum, t); err != nil {
-		return fmt.Errorf("invalid digest format")
+	b := make([]byte, hex.DecodedLen(len(t)))
+	if _, err := hex.Decode(b, t); err != nil {
+		return &DigestError{
+			msg:   "unable to decode digest as hex",
+			inner: err,
+		}
 	}
+	return d.setChecksum(b)
+}
+
+// DigestError is the concrete type backing errors returned from Digest's
+// methods.
+type DigestError struct {
+	msg   string
+	inner error
+}
+
+// Error implements error.
+func (e *DigestError) Error() string {
+	return e.msg
+}
+
+// Unwrap enables errors.Unwrap.
+func (e *DigestError) Unwrap() error {
+	return e.inner
+}
+
+func (d *Digest) setChecksum(b []byte) error {
+	var sz int
+	switch d.algo {
+	case "sha256":
+		sz = sha256.Size
+	case "sha512":
+		sz = sha512.Size
+	default:
+		return &DigestError{msg: fmt.Sprintf("unknown algorthm %q", d.algo)}
+	}
+	if l := len(b); l != sz {
+		return &DigestError{msg: fmt.Sprintf("bad checksum length: %d", l)}
+	}
+	d.checksum = b
 	return nil
 }
 
@@ -51,7 +110,7 @@ func (d *Digest) UnmarshalText(t []byte) error {
 func (d *Digest) Scan(i interface{}) error {
 	s, ok := i.(string)
 	if !ok {
-		return fmt.Errorf("invalid digest type")
+		return &DigestError{msg: "invalid digest type"}
 	}
 	return d.UnmarshalText([]byte(s))
 }
@@ -65,13 +124,15 @@ func (d Digest) Value() (driver.Value, error) {
 	return string(b), nil
 }
 
-func NewDigest(algo string, sum []byte) Digest {
-	return Digest{
-		algo:     algo,
-		checksum: sum,
+// NewDigest constructs a Digest.
+func NewDigest(algo string, sum []byte) (Digest, error) {
+	d := Digest{
+		algo: algo,
 	}
+	return d, d.setChecksum(sum)
 }
 
+// ParseDigest constructs a Digest from a string, ensuring it's well-formed.
 func ParseDigest(digest string) (Digest, error) {
 	d := Digest{}
 	return d, d.UnmarshalText([]byte(digest))
